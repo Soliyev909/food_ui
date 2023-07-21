a21dd3210336aa5a9c97956ec02d9c4e8a8375e7
SamandarTakhirov <itbloog741@gmail.com>
2023-07-21 16:48:59 +0500

    update_images

---
+++ lib/src/common/constants/app_icons.dart
@@ -0,0 +1,23 @@
+abstract class AppImages{
+  static const ic_camera = "assets/images/icons/ic_camera.png";
+  static const ic_crown = "assets/images/icons/ic_crown.png";
+  static const ic_curve = "assets/images/icons/ic_curve.png";
+  static const ic_favorite = "assets/images/icons/ic_favorit.png";
+  static const ic_favorit_bold = "assets/images/icons/ic_favorit_bold.png";
+  static const ic_galery = "assets/images/icons/ic_frame.png";
+  static const ic_home = "assets/images/icons/ic_home.png";
+  static const ic_home_bold = "assets/images/icons/ic_home_bold.png";
+  static const ic_lightning = "assets/images/icons/ic_lightning.png";
+  static const ic_logout = "assets/images/icons/ic_logout.png";
+  static const ic_paper = "assets/images/icons/ic_paper.png";
+  static const ic_play = "assets/images/icons/ic_play.png";
+  static const ic_profile = "assets/images/icons/ic_profile.png";
+  static const ic_refresh = "assets/images/icons/ic_refresh.png";
+  static const ic_scan = "assets/images/icons/ic_scan.png";
+  static const ic_search = "assets/images/icons/ic_search.png";
+  static const ic_search_bold = "assets/images/icons/ic_search_bold.png";
+  static const ic_setting = "assets/images/icons/ic_setting.png";
+  static const ic_star = "assets/images/icons/ic_star.png";
+  static const ic_user = "assets/images/icons/ic_user.png";
+  static const ic_user_bold = "assets/images/icons/ic_user_bold.png";
+}
